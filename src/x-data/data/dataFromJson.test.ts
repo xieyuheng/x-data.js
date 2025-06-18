5cad2f1d294f7527f9b21b6ed812b59f7f0a5268
Xie Yuheng <xyheme@gmail.com>
2025-06-19 02:11:09 +0800

    up

--- src/x-data/data/dataFromJson.test.ts
+++ src/x-data/data/dataFromJson.test.ts
@@ -1,17 +1,22 @@
 import assert from "node:assert"
 import { test } from "node:test"
-import { dataFromJson } from "../data/index.js"
 import * as X from "../data/index.js"
+import { dataFromJson } from "../data/index.js"
 
 test("dataFromJson", () => {
   assert.deepStrictEqual(dataFromJson("abc"), X.String("abc"))
 
-  assert.deepStrictEqual(dataFromJson(["a", "b", "c"]),
-                         X.List([X.String("a"), X.String("b"), X.String("c")]))
+  assert.deepStrictEqual(
+    dataFromJson(["a", "b", "c"]),
+    X.List([X.String("a"), X.String("b"), X.String("c")]),
+  )
 
-  assert.deepStrictEqual(dataFromJson({ a: 1, b: 2, c: 3 }), X.Record({
-    a: X.Int(1),
-    b: X.Int(2),
-    c: X.Int(3),
-  }))
+  assert.deepStrictEqual(
+    dataFromJson({ a: 1, b: 2, c: 3 }),
+    X.Record({
+      a: X.Int(1),
+      b: X.Int(2),
+      c: X.Int(3),
+    }),
+  )
 })
