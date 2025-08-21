92a731c50b446f30e4c4c69c2751813e6af77208
Xie Yuheng <xyheme@gmail.com>
2025-08-21 08:31:52 +0800

    [repl] `bracketBalance`

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
+[repl] `Repl.demo.ts`
 [repl] `Repl` -- has `commitedTokens`
 [repl] `Repl` -- has `currentTokens`
 [repl] `replStart` -- handle `line` event of readline
--- src/repl/Repl.ts
+++ src/repl/Repl.ts
@@ -2,6 +2,8 @@ import assert from "node:assert"
 import { stdin, stdout } from "node:process"
 import * as Readline from "node:readline"
 import { type Data } from "../data/index.ts"
+import { LexerConfig } from "../lexer/index.ts"
+import { type Token } from "../token/index.ts"
 
 type Repl = {
   onData: (data: Data) => Promise<void>
@@ -22,4 +24,27 @@ export function replStart(repl: Repl): void {
 export function replEnd(repl: Repl): void {
   assert(repl.rl)
   repl.rl.close()
+}
+
+type Balance = "Wrong" | "Ok" | "Perfect"
+
+function bracketBalance(config: LexerConfig, tokens: Array<Token>): Balance {
+  const bracketStack: Array<string> = []
+  for (const token of tokens) {
+    if (token.kind === "BracketStart") {
+      bracketStack.push(token.value)
+    }
+
+    if (token.kind === "BracketEnd") {
+      const start = bracketStack.pop()
+      if (start === undefined) return "Wrong"
+      if (!config.matchBrackets(start, token.value)) return "Wrong"
+    }
+  }
+
+  if (bracketStack.length === 0) {
+    return "Perfect"
+  } else {
+    return "Ok"
+  }
 }
